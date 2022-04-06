f12a3b5a6b6a9881af6a2045ca0cd815015a30b9
Abhishek P <abhirajp075@gmail.com>
2022-04-06 23:09:38 +0530

    C1

---
+++ C_2_1.py
@@ -0,0 +1,14 @@
+def birth(m):
+  a=0
+  b=1
+  print(" {:s}\n{:s}\n{:s}{:s}{:s}{:s}{:s}".format((22)*"_","|  Month |\tPairs  |","|",(8)*"-","|",(13)*"-","|"))
+  for i in range(m):
+    temp=b
+    b=a+b
+    a=temp
+    if(i==m-1):
+        print(print("{:s}{:d}{:s}{:s}{:s}{:d}{:s}\n{:s}".format("|  ",i+1,"\t"," |","\t",a,"      |",(22)*"-")))
+    else:
+        print("{:s}{:d}{:s}{:s}{:s}{:d}{:s}\n{:s}{:s}{:s}{:s}{:s}".format("|  ",i+1,"\t"," |","\t",a,"      |","|",(8)*"-","|",(13)*"-","|"))
+month=int(input("Enter the number of months : "))
+birth(month)
