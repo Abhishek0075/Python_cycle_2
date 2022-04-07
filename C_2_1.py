5e294b09d7c785aa526d14b2f7e119d4e2a52f62
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2022-04-07 09:54:39 +0530

    update

--- C_2_1.py
+++ C_2_1.py
@@ -7,7 +7,7 @@ def birth(m):
     b=a+b
     a=temp
     if(i==m-1):
-        print(print("{:s}{:d}{:s}{:s}{:s}{:d}{:s}\n{:s}".format("|  ",i+1,"\t"," |","\t",a,"      |",(22)*"-")))
+        print(print("{:s}{:d}{:s}{:s}{:s}{:d}{:s}\n {:s}".format("|  ",i+1,"\t"," |","\t",a,"      |",(22)*"-")))
     else:
         print("{:s}{:d}{:s}{:s}{:s}{:d}{:s}\n{:s}{:s}{:s}{:s}{:s}".format("|  ",i+1,"\t"," |","\t",a,"      |","|",(8)*"-","|",(13)*"-","|"))
 month=int(input("Enter the number of months : "))
